a5535feedec807dc55d10e4a9b0863b39fc0d07f
Lanjkn <86944753+Lanjkn@users.noreply.github.com>
2022-05-10 19:09:20 -0300

    icons-atualização

---
+++ php/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+$con = mysqli_connect("localhost","root","","database");
+
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+  }
+
+
+?>
