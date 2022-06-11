59260c9a903e15ed09b112552421bcbac917bb99
willin <pacovicz_@outlook.com>
2022-06-11 18:28:33 -0300

    AA
    AA

--- sql/sql-inicial.sql
+++ sql/sql-inicial.sql
@@ -26,11 +26,11 @@ INSERT INTO produtos (nome, imagem, preco)
 VALUES 
 ("Men's Jacket","http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/mens-better-than-naked-jacket-AVMH_LC9_hero.png", 19.00),
 ("Women's Jacket","http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/womens-better-than-naked-jacket-AVKL_NN4_hero.png", 29.00),
-("Enduro&reg; Hydration Pack",'http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/enduro-boa-hydration-pack-AJQZ_JK3_hero.png', 124.00),
+("Hydration Pack",'http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/enduro-boa-hydration-pack-AJQZ_JK3_hero.png', 124.00),
 ("Men's Slim Fit Shirt","https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg", 19.00),
 ("Mens Cotton Jacket",'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg', 139.00),
 ("Men's Casual Slim Fit",'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg', 23.00),
-("Women's Chain Bracelet",'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg', 12.00),
+("Women's Bracelet",'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg', 12.00),
 ("Women's T-Shirt",'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg', 44.00),
 ("Women's Shirt",'https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg', 42.00),
 ("Women's Shirt",'https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg', 87.00),
